3da52f665ba45874315f493ce67344d966cb0309
KTsaneff <NECtarinepaper157>
2021-02-09 16:30:26 +0200

    csfund firstatt

---
+++ CSharpFundamentals/204.TribonacciSequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _204.TribonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[3] { 1, 1, 2 };
+            int[] outputArr = new int[limit];
+
+            for (int i = 0; i < arr.Length && i< outputArr.Length; i++)
+            {
+                outputArr[i] = arr[i];
+            }
+
+            if (limit > arr.Length)
+            {
+                for (int j = 3; j < outputArr.Length; j++)
+                {
+                    outputArr[j] = outputArr[j - 1] + outputArr[j - 2] + outputArr[j - 3];
+                }
+            }
+            
+
+            Console.WriteLine(string.Join(" ", outputArr));
+        }
+    }
+}
